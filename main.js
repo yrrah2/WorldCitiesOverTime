a86d908bdc2d06b3880ebee8c5f6b63142accab0
Harry Hackett <yrrah2@users.noreply.github.com>
2019-12-12 15:49:49 +1300

    Console log test

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ const controls = {
 };
 gui.add(controls, 'Cities').onChange(enabled => d3.selectAll('.city').style('display', enabled ? null : 'none'));
 gui.add(controls, 'Graticule Grid').onChange(enabled => d3.selectAll('.graticule').style('display', enabled ? null : 'none'));
-gui.add(controls, 'Voronoi Layer').onChange(enabled => d3.selectAll('.voronoi').style('display', enabled ? null : 'none'));
+gui.add(controls, 'Voronoi Layer').onChange(enabled => console.log(controls); d3.selectAll('.voronoi').style('display', enabled ? null : 'none'));
 gui.add(controls, "Year").min(1500).max(2020).step(10);
 
 const width = window.innerWidth;
