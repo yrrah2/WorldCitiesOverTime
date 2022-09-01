62383b5fd3af7d861a8a2d04ad4c6822ffe89462
Harris Hackett <elderyrrah@gmail.com>
2022-09-01 16:01:12 +1200

    Updated regime list

--- scan.py
+++ scan.py
@@ -1,12 +1,14 @@
 # Takes cities.json and runs through all the different regimes mentioned.
 # Compiles a complete list of all regimes.
 
+import codecs
 import json
 contents = []
 
 try:
     with open("cities.json", 'r') as f:
-        contents = json.load(f)
+        d = codecs.open('cities.json', 'r', 'utf-8-sig')
+        contents = json.load(d)
 except Exception as e:
     print(e)
 
@@ -18,5 +20,9 @@ for date in dates:
         event = entry['event']
         if event not in regimes:
             regimes.append(event)
+
+with open("regimes.json", 'w') as f:
+    s = json.dumps(regimes)
+    f.write(s)
 
 print(regimes)
