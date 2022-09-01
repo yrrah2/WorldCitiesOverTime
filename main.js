aad85f85d9cb8a3d1f49059643e3ebf13603942c
Harry Hackett <yrrah2@users.noreply.github.com>
2022-09-01 14:19:54 +1200

    test moving promise to beginning

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const MAX_URQUHART_DISTANCE = 0.15; // geo radians
-
+Promise.all([
+  fetch('https://yrrah2.github.io/WorldCitiesOverTime/ocean.json').then(r => r.json()),
+  fetch('https://yrrah2.github.io/WorldCitiesOverTime/cities.json').then(r => r.json()),
+  fetch('https://yrrah2.github.io/WorldCitiesOverTime/regimes.json').then(r => r.json())
+]).then(([world, cities, regimes]) => {
 var regime_colors = {"No one": "#353535"};
 regimes.forEach(regime => regime_colors[regime] = getRandomColor());
 
@@ -151,11 +155,7 @@ d3.geoZoom()
   .onMove(render)
   (svg.node());
 
-Promise.all([
-  fetch('https://yrrah2.github.io/WorldCitiesOverTime/ocean.json').then(r => r.json()),
-  fetch('https://yrrah2.github.io/WorldCitiesOverTime/cities.json').then(r => r.json()),
-  fetch('https://yrrah2.github.io/WorldCitiesOverTime/regimes.json').then(r => r.json())
-]).then(([world, cities, regimes]) => {
+
     
   // Sphere (Land)
   svg.append('path').attr('class', 'geo sphere')
