46fceb74873268d35189f8e49808d00d947dda5d
Harry Hackett <yrrah2@users.noreply.github.com>
2022-09-02 16:58:40 +1200

    just use this before remove d3-tip

--- main.js
+++ main.js
@@ -204,7 +204,7 @@ d3.geoZoom()
             .enter().append('path')
             .attr('class', 'geo voronoi')
             .attr("id", d => d.city)
-            .on('mouseover', ({properties: { site: d }}, i, n) => { tip.show(getCityDesc(d), n[i])})
+            .on('mouseover', ({properties: { site: d }}) => { tip.show(getCityDesc(d), this)})
             .on('mouseout', tip.hide);
         
         // Ocean overlay
