b3c9f767aa84b72373e6105d2ef0a88caac82c84
Harry Hackett <yrrah2@users.noreply.github.com>
2022-09-07 19:15:21 +1200

    ;

--- main.js
+++ main.js
@@ -225,7 +225,7 @@ d3.geoZoom()
                 if ( area.properties.site.dates.length > 0 ) {
                     let regime = recentEvent(area.properties.site.dates);
                     this.style.fill = regime_colors[regime.toString()];
-                    this.setAttribute("class", this.className.baseVal + ' ' + regime.replace(/ /g,"_"););
+                    this.setAttribute("class", this.className.baseVal + ' ' + regime.replace(/ /g,"_"));
                 }
             }
         );
