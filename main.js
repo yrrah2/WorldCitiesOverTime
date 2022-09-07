b3141ac9afb11415b90608b786df57ead72ee6f8
Harry Hackett <yrrah2@users.noreply.github.com>
2022-09-07 18:20:48 +1200

    log it x2

--- main.js
+++ main.js
@@ -225,7 +225,7 @@ d3.geoZoom()
                 if ( area.properties.site.dates.length > 0 ) {
                     let regime = recentEvent(area.properties.site.dates);
                     this.style.fill = regime_colors[regime.toString()];
-                    console.log(this);
+                    console.log(this.className);
                     this.setAttribute("class", this.className + regime.toString());
                 }
             }
