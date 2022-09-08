1f528e2f2a28f85c833e45796d3dc7effa4d57ea
Harry Hackett <yrrah2@users.noreply.github.com>
2022-09-08 21:12:06 +1200

    old system

--- main.js
+++ main.js
@@ -185,7 +185,7 @@ d3.geoZoom()
                     type: "Feature",
                     geometry: {
                         type: "Polygon",
-                        coordinates: [[]]
+                        coordinates: []
                     },
                     properties: {"regime": regime}
                 });
@@ -195,12 +195,11 @@ d3.geoZoom()
         voronoi.polygons().features.forEach(item => {
             let regime_index = indexes[recentEvent(item.properties.site.dates)];
             if(regime_index==1){console.log(item.geometry.coordinates)};
-            item.geometry.coordinates[0].forEach(coord => {
-                if (coll[regime_index].geometry.coordinates[0].indexOf(coord) === -1){
-                    coll[regime_index].geometry.coordinates[0].push(coord);
+            item.geometry.coordinates.forEach(coord => {
+                if (coll[regime_index].geometry.coordinates.indexOf(coord) === -1){
+                    coll[regime_index].geometry.coordinates.push(coord);
                 }
             });
-            coll[regime_index].geometry.coordinates[0].push(coll[regime_index].geometry.coordinates[0][0]);
         });
         
         // Geometry coords
