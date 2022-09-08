dffdd22a25b821cc86ad578f9a4250732aa418d8
Harry Hackett <yrrah2@users.noreply.github.com>
2022-09-08 22:22:24 +1200

    log it

--- main.js
+++ main.js
@@ -210,11 +210,15 @@ d3.geoZoom()
         var first = coll[re_index].geometry.coordinates[0];
         var second = coll[re_index].geometry.coordinates[1];
         
+        console.log(first);
+        console.log(first.length);
+        
         for (var i = 0; i < first.length || end == 1; i++ ) {
              for (var j = 0; j < second.length || end == 1; j++ ) {
                  if (first[i] == second[j]){
                      end = 1;
                      let combined_object = [first.slice(0,i+1), second.slice(j+1,-1), second.slice(0,j-1), first.slice(i+1, -1).concat(first[0])];
+                     console.log(combined_object);
                      coll[re_index].geometry.coordinates[0] = combined_object;
                  }
              }
